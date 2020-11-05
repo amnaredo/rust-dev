8eeb14d822ced218aa7bb00a99913e84110b5317
Alejandro <amnaredo@gmail.com>
2020-11-05 19:17:14 +0100

    Enums

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,27 @@ impl Server {
         println!("Listening on {}", self.addr);
     }
 }
+
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: String,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
