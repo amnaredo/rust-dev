c5835c7a5584a0de39f0954a8027808e517fca35
Alejandro <amnaredo@gmail.com>
2020-11-06 18:50:45 +0100

    Parsing requests

--- src/http/request.rs
+++ src/http/request.rs
@@ -17,27 +17,32 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1mHTTP/1.1
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        match str::from_utf8(buf) {
-            Ok(request) => {},
-            Err(_) => return Err(ParseError::InvalidEncoding),
-        }
-
-        match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
-            Ok(request) => {},
-            Err(e) => return Err(e),
-        }
-
         let request = str::from_utf8(buf)?;
+    
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
 
         unimplemented!();
     }
 }
 
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i+1..]))
+        }
+    }
 
+    None
+}
 
 pub enum ParseError {
     InvalidRequest,
